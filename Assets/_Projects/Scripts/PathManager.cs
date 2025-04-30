c00863a2d6c4794b72e2755f070cec15a3025d8e
NG MING SHENG <46553576+Alexdrassasin@users.noreply.github.com>
2025-04-30 11:57:14 +0800

    savepoint

--- Assets/_Projects/Scripts/PathManager.cs
+++ Assets/_Projects/Scripts/PathManager.cs
@@ -64,7 +64,7 @@ public class PathManager : MonoBehaviour
                     UpdateLineColors();
                 }
 
-                UpdateLineColors();
+                //UpdateLineColors();
             }
             else
             {
